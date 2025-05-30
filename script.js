7789e94686a0a8d9ec9ddfb308390a899530c5ac
amantoor17 <amantoordpps@gmail.com>
2025-05-30 11:58:13 +0530

    jQuery updated Navbar

--- script.js
+++ script.js
@@ -16,24 +16,28 @@ function prevSlide() {
   showSlide(currentSlide);
 }
 
-$(document).ready(function () {
-  showSlide(currentSlide);
-  setInterval(nextSlide, 3000);
-
-  $(".next").click(nextSlide);
-  $(".prev").click(prevSlide);
-});
-
-
 function toggleNav() {
   const navLinks = document.getElementById("navLinks");
   navLinks.classList.toggle("active");
 }
 
 // Close menu when clicking a link
-document.querySelectorAll(".nav-links a").forEach(link => {
-  link.addEventListener("click", () => {
-    const navLinks = document.getElementById("navLinks");
-    navLinks.classList.remove("active");
-  });
+$(".nav-links a").on("click", function() {
+  $("#navLinks").removeClass("active");
 });
+
+$(document).ready(function () {
+
+  showSlide(currentSlide);
+  setInterval(nextSlide, 3000);
+
+  $(".next").click(nextSlide);
+  $(".prev").click(prevSlide);
+
+  $(".nav-burger").click(toggleNav);
+
+});
+
+
+
+
